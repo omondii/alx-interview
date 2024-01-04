8d4a9f48a8f2a12d842f4255242f5606f4cbc534
Brian Omondi <omondiibrian00@gmail.com>
2024-01-04 12:15:18 +0300

    requirement fix-pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,8 @@
 Create a function def pascal_triangle(n): that returns a list of lists of
 integers representing the Pascal’s triangle of n:
 """
+
+
 def pascal_triangle(n):
     """ Use nested loop to generate the triangle """
     triangle = []
