7febf84ce41d1bbca6ac948692ab03ddde4f7a69
Brian Omondi <omondiibrian00@gmail.com>
2024-01-11 21:36:06 +0300

    proper commenting

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,9 +1,11 @@
 #!/usr/bin.python3
 """ canUnlockAll func that checks if boxes are all unlocked """
+
+
 def canUnlockAll(boxes):
     """ Method to check if all boxes can be opened """
-    for key in range(1, len(boxes)): #Loop to iterate over nos. from 1 to n - 1
-        flag = False #Tracks if a key can open a box
+    for key in range(1, len(boxes)):  # Loop to iterate over nos. from 1 to n - 1
+        flag = False   # Tracks if a key can open a box
         for box in range(len(boxes)):
             if key in boxes[box] and box != key:
                 flag = True
