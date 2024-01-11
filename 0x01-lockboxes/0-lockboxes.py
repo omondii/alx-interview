836429f8ba4bfbae7cbf75d2310504ad8b00c16d
Brian Omondi <omondiibrian00@gmail.com>
2024-01-11 21:39:52 +0300

    shebang update 2

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,7 +4,7 @@
 
 def canUnlockAll(boxes):
     """ Method to check if all boxes can be opened """
-    for key in range(1, len(boxes)):  # Loop to iterate over nos. from 1 to n - 1
+    for key in range(1, len(boxes)):
         flag = False   # Tracks if a key can open a box
         for box in range(len(boxes)):
             if key in boxes[box] and box != key:
