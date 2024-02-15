b756eed5b86578ee5393d666e6b2f5aa41c1c17a
Brian Omondi <omondiibrian00@gmail.com>
2024-02-15 16:40:26 +0300

    Starwars api

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/home/brian/.nvm/versions/node/v21.2.0/bin/node
+// Consuming the starwars api
+const request = require('request')
+
+const movieId = process.argv[2]
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request(url, (error, response, body) => {
+    if (error) console.log(error)
+
+    console.log(response.statusCode);
+    console.log(body);
+
+    const text = JSON.parse(response);
+    console.log(text);
+
+    const characters = test['characters']
+    for (const character in characters){
+        names = character['name'];
+        if (names) {
+            console.log(names);
+        }
+    }
+})
