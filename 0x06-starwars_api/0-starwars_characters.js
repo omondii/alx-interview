ceb68db5a45ce2b25202419dbb903f84dc7256d2
Brian Omondi <omondiibrian00@gmail.com>
2024-02-19 00:44:17 +0300

    semistd fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,42 +1,42 @@
 #!/usr/bin/node
 // Consuming the starwars api
 const starwars = () => {
-    return new Promise((resolve, reject) => {
-        const request = require('request');
+  return new Promise((resolve, reject) => {
+    const request = require('request');
 
-        const movieId = process.argv[2];
-        const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+    const movieId = process.argv[2];
+    const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-        request(url, (error, response, body) => {
-        if(response.statusCode !== 200){
-            reject(error)
-        }
-        const data = JSON.parse(body);
-        const CharUrls = data.characters;
-        const charPromise = CharUrls.map(char => {
-            return new Promise((resolve, reject) => {
-                request(char, (error, response, body) => {
-                    if (error){
-                        reject(error);
-                    } else {
-                        const charData = JSON.parse(body);
-                        resolve(charData.name);
-                    }
-                });
-            });
+    request(url, (error, response, body) => {
+      if (response.statusCode !== 200) {
+        reject(error);
+      }
+      const data = JSON.parse(body);
+      const CharUrls = data.characters;
+      const charPromise = CharUrls.map(char => {
+        return new Promise((resolve, reject) => {
+          request(char, (error, response, body) => {
+            if (error) {
+              reject(error);
+            } else {
+              const charData = JSON.parse(body);
+              resolve(charData.name);
+            }
+          });
         });
-        Promise.all(charPromise)
-            .then(characters => resolve(characters))
-            .catch(error => reject(error));
-        });
+      });
+      Promise.all(charPromise)
+        .then(characters => resolve(characters))
+        .catch(error => reject(error));
     });
+  });
 };
 starwars().then(characters => {
-    characters.forEach(character => {
-        console.log(character);
-    });
+  characters.forEach(character => {
+    console.log(character);
+  });
 }).catch(error => {
-    console.log(error);
+  console.log(error);
 });
 /*
 -> Using callbacks to display received response
