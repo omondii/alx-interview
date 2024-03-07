928e435df9c9cbb2b24f8196fcccbcdf4eceea6d
Brian Omondi <omondiibrian00@gmail.com>
2024-03-07 09:04:23 +0300

    pycode satisfied

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,15 +4,17 @@
     1 represents land
     Each cell is square, with a side length of 1
 """
+
+
 def island_perimeter(grid):
     """ Find perimeter from a list """
-    island_size = sum([i for l in grid for i in l])
+    island_size = sum([i for list in grid for i in list])
     perimeter = island_size * 2 + 2
 
     # correct for inner tiles
     for i in range(len(grid) - 1):
         for j in range(len(grid[i]) - 1):
-            if (grid[i][j] == 1 and grid[i+1][j] == 1 
+            if (grid[i][j] == 1 and grid[i+1][j] == 1
                     and grid[i][j+1] == 1 and grid[i+1][j+1] == 1):
                 perimeter -= 2
 
